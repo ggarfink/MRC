a05ff35cceadf9a07208c6a301f000d60f26efb6
ggarfink <ggarfink@umich.edu>
2020-05-30 11:37:30 -0400

    trying to push initial files again

---
+++ src/folium_stuff.py
@@ -0,0 +1,66 @@
+import folium
+import pandas as pd
+import branca
+import requests
+import json
+
+m = folium.Map(
+    location=[39, -99,],
+    zoom_start = 4.5,
+    tiles = 'cartodbpositron'
+)
+
+#can make interactive popups throughout the map - may want a for loop that can go through
+#each county on the map and put a pop-up (or each state - per county may be too busy)
+
+#read in topo_json and county_data
+topo_url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
+county_geo = f'{topo_url}/us_counties_20m_topo.json'
+county_data = './jhu_covid19_filled.csv'
+df = pd.read_csv(county_data, parse_dates=['dt'])
+df = df[df['dt'] == '5/9/2020']
+
+#issue: json isn't formatted correctly. should try using the data from our json
+folium.Choropleth(
+    geo_data = county_geo,
+    data = df,
+    columns = ['FIPS', 'Deaths'],
+    key_on = 'feature.id',
+    fill_color ='YlGn',
+    fill_opacity = 0.7,
+    line_opacity = 0.2,
+    legend_name = 'Deaths from Coronavirus'
+).add_to(m)
+
+#set the colorscale
+# colorscale = branca.colormap.linear.YlGnBu_09.scale(0, 30)
+
+# #collect the desired data from the pandas file
+# # df.fips = df.fips.astype(int)
+# # df.deaths = df.deaths.astype(int)
+# df.FIPS = df.FIPS.astype(int)
+# df.Deaths = df.Deaths.astype(int)
+# death_series = df[df['dt'] == '5/9/2020']
+# death_series = death_series.set_index('FIPS')[['Deaths']]
+
+# # #death_series.dropna(inplace=True)
+# print(death_series)
+
+#Main issue: only grabs the FIPs value, not the deaths value. can't figure out how to change it
+# #stylefunction used to color the choropleth
+# def style_function(feature):
+#     death = death_series.get(death_series, int(feature['id'][-5:]))
+#     print(death)
+#     return {
+#         'fillOpacity': 0.5,
+#         'weight': 0,
+#         'fillColor': '#black' if death is None else colorscale(death)
+#     }
+
+# folium.TopoJson(
+#     json.loads(requests.get(county_geo).text),
+#     'objects.us_counties_20m',
+#     style_function=style_function
+# ).add_to(m)
+
+m.save('index.html')
---
+++ src/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Background from './human_factors.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import L from 'leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import styled from 'styled-components';
+//import {ComposableMap, Geographies, Geography} from "react-simple-maps";
+
+//make a wrapper for the map to render
+const Wrapper = styled.div`
+    width: ${props => props.width};
+    height: ${props => props.height};
+`;
+
+//creates an example map of the US with a sick dark theme
+export default class SimpleExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 42.2808,
+      long: -83.7430, 
+      zoom: 10,
+    };
+  }
+
+  componentDidMount() {
+    this.map = L.map(`map`, {
+      center: [this.state.lat, this.state.long],
+      zoom: this.state.zoom
+    });
+
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20,
+      }).addTo(this.map);
+  }
+
+  render() {
+    return <Wrapper width='1200px' height='700px' id='map' />;
+  }
+}
+
+//loads in the background picture (human factors thing)
+var bground = {
+    backgroundImage: `url(${Background})`,
+    width: "100%",
+    height: "8000px",
+    backgroundAttachment: "scroll",
+  };
+  
+  //sets the background image to the "human factors" theme image
+  class BACKGROUND_IMAGE extends React.Component {
+    render() {
+      return(
+        <div style={bground}>
+        </div>
+      );
+    }
+  }
+
+  //this creates a basic world map using react-simple-maps: a viable option but not what we
+  //are likely going to use
+  // const geoURL = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+  // //creates a very simple map of the US by county
+  // class USAmap extends React.Component {
+  //   render() {
+  //     return(
+  //       <div>
+  //         <ComposableMap>
+  //           <Geographies geography={geoURL}>
+  //             {({geographies}) =>
+  //               geographies.map(geography => <Geography key = {geography.rsmKey} geography = {geography} />
+  //             )}
+  //           </Geographies>
+  //         </ComposableMap>
+  //       </div>
+  //     );
+  //   }
+  // }
+  
+  // //this can render all of the classes in a row
+  // class WrapperComponent extends React.Component {
+  //   render() {
+  //     return(
+        
+  //     );
+  //   }
+  // }
+
+ReactDOM.render(
+    <div>
+    <BACKGROUND_IMAGE />    
+    </div>,
+    document.getElementById('root')
+);
