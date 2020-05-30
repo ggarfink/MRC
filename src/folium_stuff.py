683f95644f5bd878b7a2c8548e982670c5cb98f5
ggarfink <ggarfink@umich.edu>
2020-05-30 14:27:38 -0400

    dont start without downloading the csv

--- src/folium_stuff.py
+++ src/folium_stuff.py
@@ -16,41 +16,20 @@ m = folium.Map(
 #read in topo_json and county_data
 topo_url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
 county_geo = f'{topo_url}/us_counties_20m_topo.json'
-county_data = './jhu_covid19_filled.csv'
-df = pd.read_csv(county_data, parse_dates=['dt'])
-df = df[df['dt'] == '5/9/2020']
-
-#issue: json isn't formatted correctly. should try using the data from our json
-# folium.Choropleth(
-#     geo_data = county_geo,
-#     data = df,
-#     columns = ['FIPS', 'Deaths'],
-#     key_on = 'feature.id',
-#     fill_color ='YlGn',
-#     fill_opacity = 0.7,
-#     line_opacity = 0.2,
-#     legend_name = 'Deaths from Coronavirus'
-# ).add_to(m)
+deaths_county_data = './jhu_covid19_filled.csv'
+death_frame = pd.read_csv(deaths_county_data, parse_dates = ['dt'] ,na_values=[' '])
 
 #set the colorscale
-colorscale = branca.colormap.linear.YlGnBu_09.scale(0, 30)
+colorscale = branca.colormap.linear.YlGnBu_09.scale(0, 100)
 
 #collect the desired data from the pandas file
-# df.fips = df.fips.astype(int)
-# df.deaths = df.deaths.astype(int)
-df.FIPS = df.FIPS.astype(int)
-df.Deaths = df.Deaths.astype(int)
-death_series = df[df['dt'] == '5/9/2020']
-death_series = death_series.set_index('FIPS')[['Deaths']]
-
-# #death_series.dropna(inplace=True)
+death_series = death_frame[death_frame['dt'] == '5/9/2020']
+death_series = death_series.set_index('FIPS')['Deaths']
 print(death_series)
 
-#Main issue: only grabs the FIPs value, not the deaths value. can't figure out how to change it
-#stylefunction used to color the choropleth
+#stylefunction used to color the choropleth. compares ids then graphs series
 def style_function(feature):
-    death = death_series.get(death_series, int(feature['id'][-5:]))
-    print(death)
+    death = death_series.get(int(feature['id'][-5:]))
     return {
         'fillOpacity': 0.5,
         'weight': 0,
